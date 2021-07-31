9329d2d7887f6f4601117c368739bce836fb9d46
Rahul Bandodkar <rahulbandodkar05@gmail.com>
2021-07-31 23:19:04 +0530

    header file added

---
+++ demo-app/src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Header() {
+  return(
+      <div className="header" >
+          <nav>
+                <div>Home</div>
+                <div>About</div>
+          </nav>
+      </div>
+  )
+}
+
+export default Header;
