9ba5e424c1b41702d6fb1dce4f7496cb6b454907
Rahul Bandodkar <rahulbandodkar05@gmail.com>
2021-08-01 00:10:58 +0530

    integrated button in footer

--- demo-app/src/components/Footer/Footer.js
+++ demo-app/src/components/Footer/Footer.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Button from '../Button/Button';
 
 function Footer() {
   return(
       <div>
           <div>Footer</div>
           <div>Contact no 9155685922</div>
+          <Button title={'Contact Us'} clickHandler={()=>console.log('clicked')}  />
       </div>
   )
 }
